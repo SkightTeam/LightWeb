417576005c48969a65a829056e19a2b3b0bdf110
Hao Wang <HWonner@gmail.com>
2012-08-21 16:12:00 -0600

    Project file impact from previous commit

--- Skight.LightWeb.Application/Startup/RegistrationScanner.cs
+++ Skight.LightWeb.Application/Startup/RegistrationScanner.cs
@@ -1,7 +1,27 @@
-﻿namespace Skight.LightWeb.Application.Startup
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Skight.LightWeb.Application.Startup
 {
-    public class RegistrationScanner
+    public class RegistrationScanner:StartupCommand
     {
-         
+        private IEnumerable<Assembly> assemblies;
+
+        public RegistrationScanner(IEnumerable<Assembly> assemblies)
+        {
+            this.assemblies = assemblies;
+        }
+
+        public void run()
+        {
+            foreach (var assembly in assemblies)
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                           
+                }
+            }
+        }
     }
 }
